dbde70feafcb287ef6a0d866d27966b624f8eaab
Jarrid Bainbridge <jarrid@jarridbainbridge.nz>
2024-07-16 22:38:42 +1200

    [bugfix] Adjust change red text button

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,22 @@
+
+// change text button
+const changeHeadingEl = document.querySelector("#change-text-btn");
+if (changeHeadingEl) {
+    document.querySelector("#change-text-btn").addEventListener("click", () => {
+        const headingEl = document.querySelector("h1");
+        const originalText = "Red Text";
+        const newText = "Bingus";
+        if (headingEl === null ) { return }
+        if (headingEl.textContent === originalText) {
+            headingEl.textContent = newText;
+            changeHeadingEl.textContent = `Revert Text to "${originalText}"`;
+        } else {
+            headingEl.textContent = originalText;
+            changeHeadingEl.textContent = `Change Text to "${newText}"`;
+        }
+    });
+}
+
 const getWeather = async() => {
     const res = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m")
     const obj = res.json()
@@ -5,9 +24,6 @@ const getWeather = async() => {
 }
 
 (() => {
-    document.querySelector("#change-text-btn").addEventListener("click", () => {
-        document.querySelector("h1").textContent = "bingus"
-    })
 
     getWeather().then(res => 
         document.querySelector("#weather-text").textContent = res
