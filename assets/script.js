3b6ee08b4047da4767bc96d577e26803334766d4
Jarrid Bainbridge <jarrid@jarridbainbridge.nz>
2024-07-16 22:40:22 +1200

    [feature] - cat image opens a modal

--- assets/script.js
+++ assets/script.js
@@ -17,15 +17,71 @@ if (changeHeadingEl) {
     });
 }
 
-const getWeather = async() => {
-    const res = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m")
-    const obj = res.json()
-    return obj["current"]["temperature_2m"]
+// weather
+
+async function getWeather() {
+    try {
+        const res = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m");
+        const data = await res.json();
+
+        if (!res.ok) {
+            throw new Error(`Error: ${res.status} ${res.statusText}`);
+        }
+        return {
+            temp: data.current.temperature_2m,
+            tempUnit: data.current_units.temperature_2m,
+        };
+    } catch (error) {
+        console.error("Failed to fetch weather data:", error);
+        return null;
+    }
+};
+
+async function updateWeatherText() {
+    // find dom element
+    const weatherText = document.querySelector("#weather-text");
+    if (!weatherText) { return }
+
+    // get temp
+    const { temp, tempUnit } = await getWeather();
+    if (temp === null) {
+        weatherText.textContent = "Unable to fetch weather data";
+        return
+    }
+    weatherText.textContent = `${temp}${tempUnit}`;
+};
+
+updateWeatherText();
+
+// modal
+
+function showModal(modal) {
+    // add eny other event based logic
+    modal.showModal();
 }
 
-(() => {
+function closeModal(modal) {
+    // add eny other event based logic
+    modal.close();
 
-    getWeather().then(res => 
-        document.querySelector("#weather-text").textContent = res
-    ).catch(e => console.error)
-})
+}
+// click outside the dialog handler
+const closeOutsideModal = ({target:modal}) => {
+    if (modal.nodeName === 'DIALOG') {
+      modal.close('dismiss')
+    }
+ }
+  
+
+const modalOpen = document.querySelector('#galleryModal-open');
+const modal = document.querySelector('#galleryModal');
+const closeModalBtn = document.querySelector('#galleryModal-close');
+if (modalOpen) {
+    modalOpen.addEventListener('click', () => showModal(modal));
+}
+if (modal) {
+    modal.addEventListener('click', closeOutsideModal)
+}
+if (closeModalBtn) {
+    closeModalBtn.addEventListener('click', () => closeModal(modal));
+}
